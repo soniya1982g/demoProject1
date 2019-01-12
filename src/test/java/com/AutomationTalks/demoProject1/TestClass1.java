1c472638196f0fc532675c7c929a0850cb709cb8
Prakash Narkhede <prakashnarkhede89@gmail.com>
2019-01-12 23:46:20 +0530

    first commit

---
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -0,0 +1,48 @@
+package com.AutomationTalks.demoProject1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+	
+	
+	public static WebDriver driver;
+	
+	
+	@BeforeMethod
+	public void launchDriver() {
+		System.setProperty("webdriver.chrome.driver", "P:\\2. Selenium Data\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+	}
+	
+	@Test
+	public void Test1() {
+		driver.navigate().to("https://automationtalks.com/");
+		System.out.println("Test 1 title is "+driver.getTitle());
+	}
+	
+	@Test
+	public void Test2() {
+		driver.navigate().to("https://automationtalks.com/");
+		System.out.println("Test 2 title is "+driver.getTitle());
+	}
+	
+	@Test
+	public void Test3() {
+		driver.navigate().to("https://automationtalks.com/");
+		System.out.println("Test 3 title is "+driver.getTitle());
+	}
+	
+	@AfterMethod
+	public void quit() {
+		driver.quit();
+		//if test case fails then log the defect in JIRA
+	}
+}
