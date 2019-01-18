45ddd56fac5452000b1865d2e0613baf093be8e7
Prakash Narkhede <prakashnarkhede89@gmail.com>
2019-01-18 23:08:36 +0530

    jira integration added

--- src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -9,9 +9,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import net.rcarz.jiraclient.BasicCredentials;
+import net.rcarz.jiraclient.Field;
+import net.rcarz.jiraclient.Issue;
+import net.rcarz.jiraclient.JiraClient;
+import net.rcarz.jiraclient.JiraException;
 
 public class TestClass1 {
 	
@@ -49,11 +57,34 @@ public class TestClass1 {
 	public void Test3() {
 		driver.navigate().to("https://automationtalks.com/");
 		System.out.println("Test 3 title is "+driver.getTitle());
+		Assert.assertEquals("Expected title", driver.getTitle());
 	}
 	
 	@AfterMethod
-	public void quit() {
+	public void quit(ITestResult result) throws JiraException {
 		driver.quit();
 		//if test case fails then log the defect in JIRA
+		if(result.getStatus() == ITestResult.FAILURE) {
+			
+			BasicCredentials creds = new BasicCredentials("admin", "admin");
+			JiraClient jira = new JiraClient("http://localhost:8081", creds);
+			Issue issueName = jira.createIssue("AUT", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName() +"is failed due to: "+ result.getThrowable().toString()).field(Field.DESCRIPTION, "get the description").execute();
+			System.out.println("Issue is created in JIRA with issue key: "+issueName.getKey());
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
