4d50762dbfa2379b44c24b462a48bd3967638dbf
soniya1982g <57792204+soniya1982g@users.noreply.github.com>
2020-12-01 10:12:38 +0530

    Update TestClass1.java

--- src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
+++ src/test/java/com/AutomationTalks/demoProject1/TestClass1.java
@@ -39,6 +39,9 @@ public class TestClass1 {
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
+		username = soniya();
+		password= 'soniya'
+			
 	}
 	
 	@Test
